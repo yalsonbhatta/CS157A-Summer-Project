518d259e2f29ec105e5e63e2fe1320d1c62d482c
jantoSJSU <66701122+jantoSJSU@users.noreply.github.com>
2020-07-24 23:11:48 +0800

    Update sqlShow.java

--- Java Servlets/sqlShow.java
+++ Java Servlets/sqlShow.java
@@ -8,22 +8,19 @@ import java.sql.*;
 
 public class sqlShow extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// Connection to MySql
-		
-		   response.setContentType("text/html");
-		      PrintWriter out = response.getWriter();
-		      String title = "Car:";
-		      
-		      String docType =
-		         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-		      
-		      out.println(docType +
-		         "<html>\n" +
-		         "<head><title>" + title + "</title></head>\n" +
-		         "<body bgcolor = \"#f0f0f0\">\n" +
-		         "<h1 align = \"center\">" + title + "</h1>\n");
-		      
+
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		String title = "Selection Results:";
+
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+		out.println(docType + "<html>\n" + "<head><title>" + title + "</title></head>\n"
+				+ "<body bgcolor = \"#f0f0f0\">\n" + "<h1 align = \"center\">" + title + "</h1>\n");
+
+		// data from frontend
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection connection = DriverManager
@@ -35,25 +32,31 @@ public class sqlShow extends HttpServlet {
 			Statement st = connection.createStatement();
 			ResultSet rs = st.executeQuery(querySql);
 			
+			String i = req.getParameter("make");
+			String j = req.getParameter("price");
+			int k = Integer.parseInt(req.getParameter("sYear"));
+			int z = Integer.parseInt(req.getParameter("lYear"));
+
 			while (rs.next()) {
-				String make = rs.getString("make"); 
-				String model = rs.getString("model"); 
-				String year = rs.getString("year"); 
-				String price = rs.getString("price"); 
+				String make = rs.getString("make");
+				String model = rs.getString("model");
+				int year = Integer.parseInt(rs.getString("year"));
+				String price = rs.getString("price");
+				if(make.equals(i) && (price.substring(0, 1)).contentEquals(j) && k<=year && year<=z) {
 				out.println("Make: " + make + "<br>");
-		        out.println("Model: " + model + "<br>");
-		        out.println("Year: " + year + "<br>");
-		        out.println("Price: " + price + "<br> + <br>");
+				out.println("Model: " + model + "<br>");
+				out.println("Year: " + year + "<br>");
+				out.println("Price: " + price + "<br> + <br>");
+				}
 			}
-	        out.println("</body></html>");
-	        st.close();
+			out.println("</body></html>");
+			st.close();
 			rs.close();
 			connection.close();
 		} catch (SQLException se) {
 			// jdbc
 			se.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
