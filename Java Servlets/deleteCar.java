4745b5028af5d15bca6eac3852e97e76b1128ab0
Justin Nguyen <justin.nguyen03@sjsu.edu>
2020-07-29 21:53:36 -0700

    New Function: Delete Car
    added deleteCar code that allows user to delete a car using car_id.
    Also updated web.xml with new servlet and servlet-mapping

---
+++ Java Servlets/deleteCar.java
@@ -0,0 +1,36 @@
+package demo;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class deleteCar extends HttpServlet{
+
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/157a?serverTimezone=UTC", "root", "root");
+			
+			String querySql = "SELECT * FROM car";
+			Statement st = connection.createStatement();
+			ResultSet rs = st.executeQuery(querySql);
+			
+			int carid = Integer.parseInt(req.getParameter("carID"));
+			
+			querySql = "DELETE FROM car WHERE car_id = '" + carid + "';";
+			Statement nst = connection.createStatement();
+			int nrs = nst.executeUpdate(querySql);
+			res.sendRedirect("aMain.html");
+			
+		} catch (SQLException se) {
+			// jdbc
+			se.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
